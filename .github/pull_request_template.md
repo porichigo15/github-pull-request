96f979f242925015045b70065b2951b38e30f5af
Porkaew <por.porkaew15@gmail.com>
2024-07-09 14:52:00 +0700

    chore: initial project

---
+++ .github/pull_request_template.md
@@ -0,0 +1,13 @@
+## Description of changes
+
+<!-- Describe your changes in detail -->
+
+## Ticket
+
+Jira Ticket:
+
+## Types of changes
+- [ ] Documents change
+- [ ] Bug fix
+- [ ] New feature
+- [ ] Unit test
---
+++ src/main/java/com/example/github_pull_request/github_pull_request/GithubPullRequestApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_pull_request.github_pull_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPullRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubPullRequestApplication.class, args);
+	}
+
+}
